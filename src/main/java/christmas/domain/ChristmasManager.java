6cd7264e69290ff258e815a4156aed8446e47fd7
ckkim817 <ckkim817@konkuk.ac.kr>
2023-11-14 20:52:55 +0900

    [Feat] 식당 방문 요일 계산 기능 구현

--- src/main/java/christmas/domain/ChristmasManager.java
+++ src/main/java/christmas/domain/ChristmasManager.java
@@ -7,6 +7,7 @@ public class ChristmasManager {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private DayOfWeekCalculator dayOfWeekCalculator;
 
     public ChristmasManager(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -15,15 +16,24 @@ public class ChristmasManager {
 
     public void run() {
         inputVisitDate();
+        if (dayOfWeekCalculator.isChristmasDDay()) {
+            System.out.println("크리스마스 디데이 할인");
+        }
+        if (dayOfWeekCalculator.isWeekend()) {
+            System.out.println("주말 할인");
+        }
+        if (dayOfWeekCalculator.isSpecialDay()) {
+            System.out.println("특별 할인");
+        }
     }
 
-    private DayOfWeekCalculator inputVisitDate() {
+    private void inputVisitDate() {
         try {
             int date = inputView.readVisitDate();
-            return new DayOfWeekCalculator(date);
+            this.dayOfWeekCalculator = new DayOfWeekCalculator(date);
         } catch (IllegalArgumentException e) {
             System.out.print("[ERROR] " + e.getMessage());
-            return inputVisitDate();
+            inputVisitDate();
         }
     }
 }
--- src/main/java/christmas/domain/DayOfWeekCalculator.java
+++ src/main/java/christmas/domain/DayOfWeekCalculator.java
@@ -7,4 +7,24 @@ public class DayOfWeekCalculator {
     public DayOfWeekCalculator(int date) {
         this.date = date;
     }
+
+    public boolean isChristmasDDay() {
+        return this.date <= 25;
+    }
+
+    public boolean isWeekend() {
+        return (this.date % 7) <= 2;
+    }
+
+    public boolean isSpecialDay() {
+        return ((this.date % 7) == 3) || (this.date == 25);
+    }
+
+    public int ChristmasDDayDiscountAmount() {
+        if (isChristmasDDay()) {
+            return 1000 + ((date - 1) * 100);
+        }
+
+        return 0;
+    }
 }
